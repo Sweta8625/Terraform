723e4fe0d484d8f6f5582745e33de34fe040dd59
Sweta8625 <130764320+Sweta8625@users.noreply.github.com>
2023-11-02 18:03:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
+
+#AWS VCP
 resource "aws_vpc" "my_vpc" {
-  cidr_block = var.vpc_cidr_block
+  cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
-    Name = var.vpc_name
+    Name = "Project VPC"
+  }
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = var.vpc_id
+  tags = {
+    Name = var.igw_name
   }
 }
