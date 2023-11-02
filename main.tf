6ed57f907db0458c8c5c5e906f1c02f344e53599
Sweta8625 <130764320+Sweta8625@users.noreply.github.com>
2023-11-02 18:48:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,10 @@ resource "aws_internet_gateway" "my_igw" {
     Name = var.igw_name
   }
 }
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = var.vpc_id
+  tags = {
+    Name = var.igw_name
+  }
+}
