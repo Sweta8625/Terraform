f71aa26826ae0a68bf273c50f124c25c1cae57c1
Sweta8625 <130764320+Sweta8625@users.noreply.github.com>
2023-11-03 19:51:08 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -10,3 +10,9 @@ variable "tag_name" {
   default     = "main"
 }
 
+variable "instance_ami" {
+  description = "Amazon Machine Image (AMI) ID for the instance"
+  type        = string
+  default     = "ami-05c0f5389589545b7"
+}
+
