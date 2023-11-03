8e7d6271e92d368b7009b8b8e3153b91f1f74fe1
Sweta8625 <130764320+Sweta8625@users.noreply.github.com>
2023-11-03 12:50:20 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "ap-south-1"
-  profile = "160071257600_PowerUserPlusRole"
+  profile = "tw-beach"
 }
+
+
